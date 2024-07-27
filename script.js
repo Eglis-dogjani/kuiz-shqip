1abad04c6b71f8dee33029d2a92b75128acb2c17
Eglis-dogjani <eglidogjani@gmail.com>
2024-07-27 10:36:44 -0700

    DOM images in javascript
    The most important part of this commit is making flags appear dynamically on storage.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let timeLeft = document.querySelector(".time-left");
 let quizContainer = document.getElementById("container");
 let nextBtn = document.getElementById("next-button");
 let countOfQuestion = document.querySelector(".number-of-question");
+let questionImage = document.getElementById('question-image');
 let displayContainer = document.getElementById("display-container");
 let scoreContainer = document.querySelector(".score-container");
 let restart = document.getElementById("restart");
@@ -14,26 +15,219 @@ let scoreCount = 0;
 let count = 11;
 let countdown;
 
+
 //Questions and Options array
 
 const quizArray = [
   {
     id: "0",
-    question: "Which is the most widely spoken language in the world?",
-    options: ["Spanish", "Mandarin", "English", "German"],
-    correct: "Mandarin",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Germany", "Albania", "Argentina", "Algeria"],
+    correct: "Albania",
+    image: "flags/al-flag.gif",
   },
   {
     id: "1",
-    question: "Which is the only continent in the world without a desert?",
-    options: ["North America", "Asia", "Africa", "Europe"],
-    correct: "Europe",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Australia", "United Kingdom", "Chile", "Iceland"],
+    correct: "United Kingdom",
+    image: "flags/uk-flag.gif",
   },
   {
     id: "2",
-    question: "Who invented Computer?",
-    options: ["Charles Babbage", "Henry Luce", "Henry Babbage", "Charles Luce"],
-    correct: "Charles Babbage",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Belgium", "Brazil", "Egypt", "Germany"],
+    correct: "Germany",
+    image: "flags/gm-flag.gif",
+  },
+  {
+    id: "3",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Russia", "Croatia", "France", "Sweden"],
+    correct: "France",
+    image: "flags/fr-flag.gif",
+  },
+  {
+    id: "4",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Mexico", "Italy", "Portugal", "Spain"],
+    correct: "Italy",
+    image: "flags/it-flag.gif",
+  },
+  {
+    id: "5",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Romania", "Andorra", "Turkey", "Venezuela"],
+    correct: "Romania",
+    image: "flags/ro-flag.gif",
+  },
+  {
+    id: "6",
+    question: "Guess the Country Flag on Asia?",
+    options: ["Saudi Arabia", "Turkey", "Vietnam", "Portugal"],
+    correct: "Turkey",
+    image: "flags/tu-flag.gif",
+  },
+  {
+    id: "7",
+    question: "Guess the Country Flag on Asia?",
+    options: ["United Kingdom", "Egypt", "China", "Croatia"],
+    correct: "China",
+    image: "flags/ch-flag.gif",
+  },
+  {
+    id: "8",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Maldives", "Spain", "Portugal", "Mexico"],
+    correct: "Spain",
+    image: "flags/sp-flag.gif",
+  },
+  {
+    id: "9",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Poland", "Monaco", "Norway", "Russia"],
+    correct: "Poland",
+    image: "flags/pl-flag.gif",
+  },
+  {
+    id: "10",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Denmark", "Austria", "Belgium", "Canada"],
+    correct: "Austria",
+    image: "flags/au-flag.gif",
+  },
+  {
+    id: "11",
+    question: "Guess the Country Flag on Asia?",
+    options: ["Mali", "India", "Ireland", "Iceland"],
+    correct: "India",
+    image: "flags/in-flag.gif",
+  },
+  {
+    id: "12",
+    question: "Guess the Country Flag on Asia?",
+    options: ["Iran", "Japan", "Pakistan", "Vietnam"],
+    correct: "Japan",
+    image: "flags/ja-flag.gif",
+  },
+  {
+    id: "13",
+    question: "Guess the Country Flag on Middle East?",
+    options: ["Cyprus", "Armenia", "Saudi Arabia", "U.A.E"],
+    correct: "Saudi Arabia",
+    image: "flags/sa-flag.gif",
+  },
+  {
+    id: "14",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Brunei", "Ukraine", "Turkey", "Canada"],
+    correct: "Ukraine",
+    image: "flags/up-flag.gif",
+  },
+  {
+    id: "15",
+    question: "Guess the Country Flag on Asia?",
+    options: ["Malaysia", "Indonesia", "United Kingdom", "United States"],
+    correct: "Malaysia",
+    image: "flags/my-flag.gif",
+  },
+  {
+    id: "16",
+    question: "Guess the Country Flag on Asia?",
+    options: ["Sudan", "Japan", "South Korea", "North Korea"],
+    correct: "South Korea",
+    image: "flags/ks-flag.gif",
+  },
+  {
+    id: "17",
+    question: "Guess the Country Flag Island?",
+    options: ["Monaco", "Australia", "United Kingdom", "Malaysia"],
+    correct: "Australia",
+    image: "flags/as-flag.gif",
+  },
+  {
+    id: "18",
+    question: "Guess the Country Flag on South America?",
+    options: ["Cuba", "Brazil", "Mexico", "Argentina"],
+    correct: "Brazil",
+    image: "flags/br-flag.gif",
+  },
+  {
+    id: "19",
+    question: "Guess the Country Flag on South America?",
+    options: ["Costa Rica", "Ecuador", "Argentina", "Venezuela"],
+    correct: "Argentina",
+    image: "flags/ar-flag.gif",
+  },
+  {
+    id: "20",
+    question: "Guess the Country Flag on America?",
+    options: ["Canada", "United States", "Malaysia", "Brazil"],
+    correct: "United States",
+    image: "flags/us-flag.gif",
+  },
+  {
+    id: "21",
+    question: "Guess the Country Flag on America?",
+    options: ["China", "Canada", "Cameroon", "Argentina"],
+    correct: "Canada",
+    image: "flags/ca-flag.gif",
+  },
+  {
+    id: "22",
+    question: "Guess the Country Flag on America?",
+    options: ["Bahamas", "Mexico", "Italy", "Jamaica"],
+    correct: "Mexico",
+    image: "flags/mx-flag.gif",
+  },
+  {
+    id: "23",
+    question: "Guess the Country Flag on South Africa?",
+    options: ["Spain", "Colombia", "Cuba", "Costa Rica"],
+    correct: "Colombia",
+    image: "flags/co-flag.gif",
+  },
+  {
+    id: "24",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Brazil", "Iceland", "Italy", "Norway"],
+    correct: "Iceland",
+    image: "flags/ic-flag.gif",
+  },
+  {
+    id: "25",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Belgium", "Ireland", "Italy", "Kuwait"],
+    correct: "Ireland",
+    image: "flags/ei-flag.gif",
+  },
+  {
+    id: "26",
+    question: "Guess the Country Flag on South America?",
+    options: ["Ecuador", "Mexico", "Colombia", "Egypt"],
+    correct: "Ecuador",
+    image: "flags/ec-flag.gif",
+  },
+  {
+    id: "27",
+    question: "Guess the Country Flag on Asia?",
+    options: ["Singapore", "Mexico", "Germany", "Tunisia"],
+    correct: "Singapore",
+    image: "flags/sn-flag.gif",
+  },
+  {
+    id: "28",
+    question: "Guess the Country Flag on South America?",
+    options: ["Zambia", "Mexico", "Uruguay", "Venezuela"],
+    correct: "Uruguay",
+    image: "flags/uy-flag.gif",
+  },
+  {
+    id: "29",
+    question: "Guess the Country Flag on Europe?",
+    options: ["Monaco", "Mexico", "Italy", "Denmark"],
+    correct: "Monaco",
+    image: "flags/mn-flag.gif",
   },
 ];
 
@@ -112,6 +306,12 @@ function quizCreator() {
     question_DIV.classList.add("question");
     question_DIV.innerHTML = i.question;
     div.appendChild(question_DIV);
+
+    //Flag Image
+        div.innerHTML +=`
+        <img id="question-image" src="${i.image}" alt="flag" width="99%">
+        `;
+        quizContainer.appendChild(div);
     //options
     div.innerHTML += `
     <button class="option-div" onclick="checker(this)">${i.options[0]}</button>
